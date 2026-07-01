08979b6df55d15dfdca07c4aa329dc16ecef2b91
norandot <norandot@gmail.com>
2026-07-01 13:49:09 +0900

    修正: T() 関数呼び出しの引用符エラーを解決
    - Get-HtmlPage 関数内のすべての T() 呼び出しをここスコープ前に評価
    - メニュー項目追加時の T() 呼び出しを位置パラメータ構文に変更
    - タイマー内のログ関数呼び出しを変数に代入してから使用

--- AllowRemoteShutdown.ps1
+++ AllowRemoteShutdown.ps1
@@ -193,7 +193,7 @@ function Safe-StartProcess {
     } catch {
         Write-Log "Start-Process failed: $file $args -> $_"
         try {
-            $icon.BalloonTipTitle = T('TrayText')
+            $icon.BalloonTipTitle = T 'TrayText'
             $icon.BalloonTipText  = "Error: $file"
             $icon.ShowBalloonTip(3000)
         } catch { }
@@ -247,25 +247,25 @@ function Update-MenuCheck {
 function Invoke-ShutdownAction {
     param($mode, $delaySeconds, $remoteEndPoint)
     $args = "$mode -f -t $delaySeconds -v 10 -c"
-    Write-Log (T('Log_Invoke', $mode, $delaySeconds, $remoteEndPoint))
+    Write-Log (T 'Log_Invoke' $mode $delaySeconds $remoteEndPoint)
 
     if ($script:cancelHandler) {
         try { $icon.remove_BalloonTipClicked($script:cancelHandler) } catch { }
     }
     $script:cancelHandler = {
         Safe-StartProcess $config.AbortCmd @($config.AbortArgs)
-        Write-Log (T('Log_AbortByUser'))
+        Write-Log (T 'Log_AbortByUser')
         try {
-            $icon.BalloonTipTitle = T('TrayText')
-            $icon.BalloonTipText  = T('AbortedBalloon')
+            $icon.BalloonTipTitle = T 'TrayText'
+            $icon.BalloonTipText  = T 'AbortedBalloon'
             $icon.ShowBalloonTip(3000)
         } catch { }
     }
     $icon.add_BalloonTipClicked($script:cancelHandler)
 
     try {
-        $icon.BalloonTipTitle = T('TrayText')
-        $icon.BalloonTipText  = T('RunningBalloon', $delaySeconds)
+        $icon.BalloonTipTitle = T 'TrayText'
+        $icon.BalloonTipText  = T 'RunningBalloon' $delaySeconds
         $icon.ShowBalloonTip([int]$delaySeconds * 1000)
     } catch { }
 
@@ -280,18 +280,19 @@ function Get-HtmlPage {
     $msgHtml = if ($message -ne "") { "<p class='msg'>$([System.Web.HttpUtility]::HtmlEncode($message))</p>" } else { "" }
     $langAttr = $script:lang
 
-    $quickLabel = [System.Web.HttpUtility]::HtmlEncode((T('QuickButton', $script:mode, $script:delay)))
-    $execLabel = [System.Web.HttpUtility]::HtmlEncode(T('ExecuteButton'))
-    $abortLabel = [System.Web.HttpUtility]::HtmlEncode(T('AbortButton'))
-    $modeOption_Shutdown = [System.Web.HttpUtility]::HtmlEncode(T('Mode_Shutdown'))
-    $modeOption_Restart  = [System.Web.HttpUtility]::HtmlEncode(T('Mode_Restart'))
-    $modeOption_Logoff   = [System.Web.HttpUtility]::HtmlEncode(T('Mode_Logoff'))
-    $modeOption_Hibernate= [System.Web.HttpUtility]::HtmlEncode(T('Mode_Hibernate'))
-    $delayOption_15 = [System.Web.HttpUtility]::HtmlEncode(T('Delay_15'))
-    $delayOption_30 = [System.Web.HttpUtility]::HtmlEncode(T('Delay_30'))
-    $delayOption_60 = [System.Web.HttpUtility]::HtmlEncode(T('Delay_60'))
-    $delayOption_Custom = [System.Web.HttpUtility]::HtmlEncode(T('Delay_Custom'))
-    $delayPlaceholder = [System.Web.HttpUtility]::HtmlEncode(T('CustomDelayPlaceholder'))
+    # ここスコープ前に全ての T() 呼び出しを評価
+    $quickLabel = [System.Web.HttpUtility]::HtmlEncode((T 'QuickButton' $script:mode $script:delay))
+    $execLabel = [System.Web.HttpUtility]::HtmlEncode((T 'ExecuteButton'))
+    $abortLabel = [System.Web.HttpUtility]::HtmlEncode((T 'AbortButton'))
+    $modeOption_Shutdown = [System.Web.HttpUtility]::HtmlEncode((T 'Mode_Shutdown'))
+    $modeOption_Restart  = [System.Web.HttpUtility]::HtmlEncode((T 'Mode_Restart'))
+    $modeOption_Logoff   = [System.Web.HttpUtility]::HtmlEncode((T 'Mode_Logoff'))
+    $modeOption_Hibernate= [System.Web.HttpUtility]::HtmlEncode((T 'Mode_Hibernate'))
+    $delayOption_15 = [System.Web.HttpUtility]::HtmlEncode((T 'Delay_15'))
+    $delayOption_30 = [System.Web.HttpUtility]::HtmlEncode((T 'Delay_30'))
+    $delayOption_60 = [System.Web.HttpUtility]::HtmlEncode((T 'Delay_60'))
+    $delayOption_Custom = [System.Web.HttpUtility]::HtmlEncode((T 'Delay_Custom'))
+    $delayPlaceholder = [System.Web.HttpUtility]::HtmlEncode((T 'CustomDelayPlaceholder'))
 
     @"
 <!DOCTYPE html>
@@ -374,7 +375,7 @@ $prefix = $config.Host -f $config.Port
 $listener = New-Object System.Net.HttpListener
 $listener.Prefixes.Add($prefix)
 $listener.Start()
-Write-Log (T('Log_ServerStarted', $prefix))
+Write-Log (T 'Log_ServerStarted' $prefix)
 
 # --- タスクトレイアイコンを設定 ---
 # Set up task tray icon
@@ -383,19 +384,19 @@ $trayIcon = Load-TrayIcon $config
 $icon = New-Object System.Windows.Forms.NotifyIcon
 $icon.Icon    = $trayIcon
 $icon.Visible = $true
-$icon.Text    = T('TrayText')
+$icon.Text    = T 'TrayText'
 
 # --- コンテキストメニューを構築 ---
 # Build context menu
 $menu = New-Object System.Windows.Forms.ContextMenu
 
-$menuMode = New-Object System.Windows.Forms.MenuItem(T('MenuMode'))
+$menuMode = New-Object System.Windows.Forms.MenuItem((T 'MenuMode'))
 $modeItems = @()
 @(
-    @{ Label = T('Mode_Shutdown'); Value = "-s" }
-    @{ Label = T('Mode_Restart');  Value = "-r" }
-    @{ Label = T('Mode_Logoff');   Value = "-o" }
-    @{ Label = T('Mode_Hibernate');Value = "-h" }
+    @{ Label = T 'Mode_Shutdown'; Value = "-s" }
+    @{ Label = T 'Mode_Restart';  Value = "-r" }
+    @{ Label = T 'Mode_Logoff';   Value = "-o" }
+    @{ Label = T 'Mode_Hibernate'; Value = "-h" }
 ) | ForEach-Object {
     $item = New-Object System.Windows.Forms.MenuItem($_.Label)
     $item.Tag     = $_.Value
@@ -404,19 +405,19 @@ $modeItems = @()
     $item.Add_Click({
         $script:mode = $itemValue
         Update-MenuCheck $modeItems $script:mode
-        Write-Log (T('Log_ModeChanged', $script:mode))
+        Write-Log (T 'Log_ModeChanged' $script:mode)
     })
     $modeItems += $item
     $menuMode.MenuItems.Add($item) | Out-Null
 }
 $menu.MenuItems.Add($menuMode) | Out-Null
 
-$menuDelay = New-Object System.Windows.Forms.MenuItem(T('MenuDelay'))
+$menuDelay = New-Object System.Windows.Forms.MenuItem((T 'MenuDelay'))
 $delayItems = @()
 @(
-    @{ Label = T('Delay_15'); Value = "15" }
-    @{ Label = T('Delay_30'); Value = "30" }
-    @{ Label = T('Delay_60'); Value = "60" }
+    @{ Label = T 'Delay_15'; Value = "15" }
+    @{ Label = T 'Delay_30'; Value = "30" }
+    @{ Label = T 'Delay_60'; Value = "60" }
 ) | ForEach-Object {
     $item = New-Object System.Windows.Forms.MenuItem($_.Label)
     $item.Tag     = $_.Value
@@ -425,7 +426,7 @@ $delayItems = @()
     $item.Add_Click({
         $script:delay = $itemValue
         Update-MenuCheck $delayItems $script:delay
-        Write-Log (T('Log_DelayChanged', $script:delay))
+        Write-Log (T 'Log_DelayChanged' $script:delay)
     })
     $delayItems += $item
     $menuDelay.MenuItems.Add($item) | Out-Null
@@ -434,7 +435,8 @@ $menu.MenuItems.Add($menuDelay) | Out-Null
 
 $menu.MenuItems.Add("-") | Out-Null
 
-$menu.MenuItems.Add(T('MenuShowLog'), {
+$menuShowLogLabel = T 'MenuShowLog'
+$menu.MenuItems.Add($menuShowLogLabel, {
     if ($config.ShowConsole) {
         [ConsoleWindow]::ShowWindow($consoleHwnd, 0) | Out-Null
         $config.ShowConsole = $false
@@ -444,7 +446,8 @@ $menu.MenuItems.Add(T('MenuShowLog'), {
     }
 }) | Out-Null
 
-$menu.MenuItems.Add(T('MenuExit'), {
+$menuExitLabel = T 'MenuExit'
+$menu.MenuItems.Add($menuExitLabel, {
     $timer.Stop()
     $listener.Stop()
     $icon.Visible = $false
@@ -481,7 +484,7 @@ $timer.Add_Tick({
                 $body = [System.Text.Encoding]::UTF8.GetBytes("Forbidden.")
                 $res.OutputStream.Write($body, 0, $body.Length)
                 $res.Close()
-                Write-Log (T('Log_RequestAuthFailed', $req.RemoteEndPoint))
+                Write-Log (T 'Log_RequestAuthFailed' $req.RemoteEndPoint)
             }
             else {
                 switch ($path) {
@@ -510,18 +513,19 @@ $timer.Add_Tick({
                     }
                     "/abort" {
                         Safe-StartProcess $config.AbortCmd @($config.AbortArgs)
-                        Write-Log (T('Log_AbortByWeb', $req.RemoteEndPoint))
+                        Write-Log (T 'Log_AbortByWeb' $req.RemoteEndPoint)
                         if ($script:cancelHandler) {
                             try { $icon.remove_BalloonTipClicked($script:cancelHandler) } catch { }
                             $script:cancelHandler = $null
                         }
                         try {
-                            $icon.BalloonTipTitle = T('TrayText')
-                            $icon.BalloonTipText  = T('AbortedBalloon')
+                            $icon.BalloonTipTitle = T 'TrayText'
+                            $icon.BalloonTipText  = T 'AbortedBalloon'
                             $icon.ShowBalloonTip(3000)
                         } catch { }
-                        $msgEncoded = [System.Web.HttpUtility]::UrlEncode(T('AbortDoneMsg'))
-                        $redirectUrl = if ($config.Token -ne "") { "/?token=$($config.Token)\u0026msg=$msgEncoded" } else { "/?msg=$msgEncoded" }
+                        $msgToEncode = T 'AbortDoneMsg'
+                        $msgEncoded = [System.Web.HttpUtility]::UrlEncode($msgToEncode)
+                        $redirectUrl = if ($config.Token -ne "") { "/?token=$($config.Token)&msg=$msgEncoded" } else { "/?msg=$msgEncoded" }
                         $res.Redirect($redirectUrl)
                         $res.Close()
                     }
@@ -534,7 +538,7 @@ $timer.Add_Tick({
                 }
             }
         } catch {
-            Write-Log (T('Log_RequestError', "$_"))
+            Write-Log (T 'Log_RequestError' "$_")
         }
 
         $script:contextTask = $listener.GetContextAsync()
@@ -553,4 +557,4 @@ $timer.Dispose()
 if ($listener.IsListening) { $listener.Stop() }
 $icon.Visible = $false
 $trayIcon.Dispose()
-Write-Log (T('Log_Cleanup'))
+Write-Log (T 'Log_Cleanup')
